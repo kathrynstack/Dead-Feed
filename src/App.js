fcd64233b4edd374e1eb7a71e476c73b93c15866
CGoddard1 <goddard.cole@gmail.com>
2023-07-01 12:17:43 -0400

    login rendered

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Login from './components/login';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dead Feed</h1>
+      <Login />
     </div>
   );
-}
+};
 
 export default App;
