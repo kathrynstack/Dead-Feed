9e2214e28902e443f2f4318c2055b82836fecb2b
CGoddard1 <goddard.cole@gmail.com>
2023-07-06 18:52:58 -0400

    login stuff

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Login from './components/login';
 
 import './App.css';
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import Footer from "./components/footer/index";
 import Header from "./components/header/index";
 import Profile from "./components/profile/index";
@@ -11,8 +11,8 @@ import Profile from "./components/profile/index";
 const App = () => {
   return (
     <div className="App">
-
-
+      <h1>Dead Feed</h1>
+      <Login />
       
       
       
