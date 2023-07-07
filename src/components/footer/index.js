bfc970e080176306524b5b1dee68f855c68d9bf4
Gabby <gabbympinto@gmail.com>
2023-07-06 21:52:43 -0400

    finished footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -11,33 +11,25 @@ import Profile from "../profile";
 
 function Footer() {
     return (
-        <div className="container py-5 h-100">
-            <div className="row d-flex justify-content-center align-items-center h-100">
-                <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                    <div className="card bg-dark text-dark">
-                        <div className="card-body p-5 text-center">
-                                <nav className="navbar">
-                                    <div className="home">
-                                        <img src={HomeIcon} alt="Home" />
-                                    </div>
 
-                                    <div className="quiz">
-                                        <img src={QuizIcon} alt="Quiz" />
-                                    </div>
-
-                                    <div className="new-post">
-                                        <img src={NewPostIcon} alt="NewPost" />
-                                    </div>
-
-                                    <div className="profile">
-                                        <img src={ProfileIcon} alt="Profile" />
-                                    </div>
-                                </nav>
-                        </div>
-                    </div>
-                </div>
+        <nav className="navbar">
+            <div className="home">
+                <img src={HomeIcon} alt="Home" />
             </div>
-        </div>
+
+            <div className="quiz">
+                <img src={QuizIcon} alt="Quiz" />
+            </div>
+
+            <div className="new-post">
+                <img src={NewPostIcon} alt="NewPost" />
+            </div>
+
+            <div className="profile">
+                <img src={ProfileIcon} alt="Profile" />
+            </div>
+        </nav>
+
     );
 }
 
--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,33 +1,33 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import "./style.css";
-import "./profilepic.png";
+import ProfilePic from "./profilepic.png";
+import EditIcon from "./edit.png";
+import Footer from "../footer";
 
 export default function Profile() {
     return (
-        <body>
             <div className="container py-5 h-100">
                 <div className="row d-flex justify-content-center align-items-center h-100">
                     <div className="col-12 col-md-8 col-lg-6 col-xl-5">
                         <div className="card bg-dark text-dark">
                             <div className="card-body p-5 text-center">
-                                <section className="profile">
+                                <img className="position-absolute fixed-top p-3" src={EditIcon} alt="Edit"></img>
                                     <div className="row login_box">
-                                        <div className="col-md-12 col-xs-12" align="center">
-                                            <img src="profilepic.png" alt="Avatar"/>
-                                            <h1 className="user-name">Gabby Pinto</h1>
+                                        <div className="col-md-12 col-xs-12 text-light" align="center">
+                                            <img className="w-50" src={ProfilePic} alt="Avatar"/>
+                                            <h1 className="user-name p-2">Gabby Pinto</h1>
                                             <span className="badges">Badges Go Here</span>
                                         </div>
-                                        <div className="" align="center">
+                                        <div className="p-3" align="center">
                                             <textarea className="bio" placeholder="About Me"></textarea>
                                         </div>
                                     </div>
-                                </section>
+                                <Footer />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </body>
     );
 }
