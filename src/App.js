78679d15e23603bab2dbe141094f95598893b5e2
kathrynstack <kathrynstack17@gmail.com>
2023-07-08 12:25:22 -0400

    Update app and landing

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Footer from "./components/footer";
 import Header from "./components/header";
 import Profile from "./components/profile";
 import Login from './components/login';
-
+import Landing from './components/landing';
 
 const App = () => {
   return (
     <div className="App">
-
-
-      <Footer />
-      
-      
-
+      <div className="container py-5 vh-100 vw-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div className="card-body p-5 text-center">
+              <div className="card bg-white text-dark">
+                <Landing />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,66 +1,30 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
-import ReactDOM from 'react-dom';
-import imageSrc from './quizbtn.png';
+import quizbtn from './quizbtn.png';
+
+var posts = [
+    { id: 1, description: "this is my fav movie" },
+    { id: 2, description: "Desc 2" },
+    { id: 3, description: "Desc 3" }
+];
 
 export default function Landing() {
     return (
-        <body style={{ backgroundColor: "red" }}>
-            <div className="container py-5 h-100">
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                        <div className="card bg-white text-dark">
-                            <div className="card-body p-5 text-center">
-
-                                <section className="landing">
-                                    <div className="top-profile">
-                                        <h1 className="title">DEAD-FEED</h1>
-                                        {/* <img>this is a placeholder for the users icon</img> */}
-                                    </div>
-                                    <div>
-                                        <h2>Categories</h2>
-                                    </div>
-                                    <div class="container">
-                                        <div class="row my-4">
-                                            <div class="col">
-                                                <h3>Chat</h3>
-                                            </div>
-                                            <div class="col">
-                                                <h3>Earn Badges</h3>
-                                            </div>
-                                            <div class="col">
-                                                <h3>Upload Photos</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <button>
-                                        <img src={imageSrc} alt="Button Image" />
-                                    </button>
-                                    <div>
-                                        <h2 >Community Chat</h2>
-                                    </div>
-                                    <div class="container">
-                                        <div class="row my-4">
-                                            <div class="col">
-                                                <h3>Favorite Slasher?</h3>
-                                            </div>
-                                            <div class="col">
-                                                <h3>Top Final Girl?</h3>
-                                            </div>
-                                            <div class="col">
-                                                <h3>Should they make another Saw? </h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </section>
-                            </div>
+        <>
+            <h2>Your Dead-Feed</h2>
+            <section className='posts'>
+                {posts.map(post => (
+                    <div className='col-md'>
+                        <div className='card bg-white text-dark py-3'>
+                            {/* <div className='card-title'>{post.title}</div> */}
+                            <img src="https://th.bing.com/th/id/OIP.yjP3lv-tTDzfsO9lZ37nUQHaG6?pid=ImgDet&rs=1"/>
+                            <div className='card-body'>{post.description}</div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </body>
+                ))}
+            </section>
+        </>
     );
 };
 
-ReactDOM.render(<Landing />, document.getElementById('root')); ReactDOM.render(<Landing />, document.getElementById('root'));
