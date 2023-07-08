b0707b7e333d8b7b34c80bdc3807ede0e6a7941d
CGoddard1 <goddard.cole@gmail.com>
2023-07-08 15:09:05 -0400

    styling for login and header

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
 import userData from '../login/userData.json';
+import Header from "../header";
 
 export default function Login() {
     const [email, setEmail] = useState('');
@@ -17,53 +18,54 @@ export default function Login() {
             console.log('Invalid credentials');
         }
     };
+
     return (
-        <body style={{ backgroundColor: "black" }}>
-            <div className="container py-5 h-100">
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                        <div className="card bg-white text-dark">
-                            <div className="card-body p-5 text-center">
-                                <section className="login">
-                                    <div class="container py-5 h-100">
-                                        <div class="row d-flex justify-content-center align-items-center h-100">
-                                            <div class="card bg-dark text-white" style={{ borderRadius: "1rem" }}>
-                                                <div class="card-body p-5 text-center">
-                                                    <div class="mb-md-5 mt-md-4 pb-5">
-                                                        <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-                                                        <form onSubmit={handleLogin}>
-                                                            <div class="form-outline form-white mb-4">
-                                                                <input type="email" id="typeEmailX" class="form-control form-control-lg"
-                                                                    value={email} onChange={(e) => setEmail(e.target.value)} required />                                                                                                                       <label class="form-label" for="typeEmailX">Email</label>
-                                                            </div>
-                                                            <div class="form-outline form-white mb-4">
-                                                                <input type="password" id="typePasswordX" class="form-control form-control-lg"
-                                                                    value={password} onChange={(e) => setPassword(e.target.value)} required />
-                                                                <label class="form-label" for="typePasswordX">Password</label>
-                                                            </div>
-                                                            <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
-                                                            <button class="btn btn-outline-light btn-lg px-2" type="submit">Login</button>
-                                                        </form>
-                                                        <div class="d-flex justify-content-center text-center mt-4 pt-1">
-                                                            <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
-                                                            <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-5"></i></a>
-                                                            <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
-                                                        </div>
-                                                    </div>
-                                                    <div>
-                                                        <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </section>
-                            </div>
+        <body class="body">
+            <section className="login">
+                <div className="login-container">
+                    <h2>Login</h2>
+                    <form onSubmit={handleLogin}>
+                        <div className="form">
+                            <input
+                                type="email"
+                                id="typeEmailX"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                            <label className="form-label" htmlFor="typeEmailX">
+                                Email
+                            </label>
                         </div>
-                    </div>
+                        <div className="form">
+                            <input
+                                type="password"
+                                id="typePasswordX"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                            <label className="form-label" htmlFor="typePasswordX">
+                                Password
+                            </label>
+                        </div>
+                        <p className="forgotPassword">
+                            <a href="#!">Forgot password?</a>
+                        </p>
+                        <button className="submit" type="submit">
+                            Login
+                        </button>
+                        <div>
+                            <p className="mb-0">
+                                Don't have an account?{' '}
+                                <a href="#!" className="text-white-50 fw-bold">
+                                    Sign Up
+                                </a>
+                            </p>
+                        </div>
+                    </form>
                 </div>
-            </div>
+            </section>
         </body>
     );
 }
