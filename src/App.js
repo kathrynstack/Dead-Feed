48f2489b933e7f899f89677ec216e966112ff27d
CGoddard1 <goddard.cole@gmail.com>
2023-07-10 18:48:13 -0400

    any changes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import Footer from "./components/footer/index";
 import Header from "./components/header/index";
 import Profile from "./components/profile/index";
 import Login from "./components/login/index";
-import "./assets/global-stylesheet/style.css";
+// import "./assets/global-stylesheet/style.css";
 
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
+// const client = new ApolloClient({
+//   uri: '/graphql',
+//   cache: new InMemoryCache(),
+// });
 
 function App() {
   return (
     <div className="App">
-      <h1>Dead Feed</h1>
+      <Header />
       <Login />
+      
       
       
       
