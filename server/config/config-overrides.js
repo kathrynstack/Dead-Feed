8740f5695210592a2c2289b765a3598e0bf7df76
kathrynstack <kathrynstack17@gmail.com>
2023-07-12 19:07:37 -0400

    Update seeds and user

--- server/config/config-overrides.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = function override(config) {
-  config.resolve.alias['../../../server/seeds/userData.json'] = path.resolve(
-    __dirname,
-    'path/to/your/userData.json'
-  );
-  return config;
-};
--- server/models/Post.js
+++ server/models/Post.js
@@ -1,27 +1,40 @@
-const { Schema } = require('mongoose');
-
+const { Schema, model } = require('mongoose');
+const imagekit = require('../config/imageKitConnection.js');
 
 const postSchema = new Schema({
-  users: [
-    {
-      type: String,
+    title: {
+        type: String,
+        required: true,
     },
-  ],
-  description: {
-    type: String,
-    required: true,
-  },
-  postId: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
+    description: {
+        type: String,
+        required: true,
+    },
+    imageLink: {
+        type: String,
+        required: true
+    }
 });
 
-module.exports = postSchema;
+// Before the delete we want to delete the posts in ImageKit
+const pre_delete = async function (next) {
+    const posts = await this.model.find(this.getQuery());
+    if (posts)
+    {
+        // const image_links = posts.map(post => post.imageLink);
+        const image_links = ['64ae270706370748f239dfe6'];
+        console.log(image_links);
+        
+        const response = await imagekit.bulkDeleteFiles(image_links).then(response => {return response});
+        console.log(response);
+        // const response = await Post.deleteMany({_id: {$in: posts}});
+    }
+    throw "ERROR";
+    next();
+};
+
+postSchema.pre('deleteMany', pre_delete);
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
