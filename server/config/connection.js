c8b136b243ede6bc25d07a077aaffd7bb072ffec
kathrynstack <kathrynstack17@gmail.com>
2023-07-11 21:24:38 -0400

    Update server info

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://powerservpa.ddns.net/deadfeed');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deadfeed');
 
 module.exports = mongoose.connection;
